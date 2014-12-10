17c41333b4ac8b3f2942c1fb7964ed52efe76270
Bhawan Singh <hello@codezyn.com>
2014-12-10 13:37:03 -0500

    Testinomials

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	 setInterval(changeQuote, 5000);
+	 var i=0;
+	
+
+	 function changeQuote(){
+	 	// $('#sliderQuotes').children('blockquote').each(function () {
+	 	    
+	 	// });
+		if(i>3)
+			i=0;
+		
+		$('#sliderQuotes').children().eq(i).fadeOut(300).next().delay(300);
+		$('#sliderQuotes').children().eq(i+1).fadeIn(300);
+		i++;
+
+		
+	 	
+
+	 }
+
+});
