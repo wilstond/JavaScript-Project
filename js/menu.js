322011107a47a7f92355d9cf6764f1119a292f19
Bhawan Singh <hello@codezyn.com>
2014-12-10 19:50:43 -0500

    menu

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
     $('#menu-content h2').next('div').hide(); 
     
     $('#menu-content h2').click(function() {
-        /*$('#menu-content h2').next('div').hide(1000);*/
-        $(this).next('div').slideToggle(1000); 
+        
+        $('#menu-content h2').not(this).next('div').slideUp(1000);
+        $(this).toggleClass( "changedContent" );
+        $(this).next('div').slideToggle(800, function(){
+       			
+   			 }); 
+
+        $('#menu-content h2').not(this).removeClass( "changedContent" );
+
+		
+
+
     });
-    
 });
