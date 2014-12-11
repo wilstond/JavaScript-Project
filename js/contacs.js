87a0cd48798bb714043b43ceeefd8d62ce84c8eb
Bhawan Singh <hello@codezyn.com>
2014-12-10 21:58:29 -0500

    contact

---
+++ js/contacs.js
@@ -0,0 +1,1 @@
+contacs.js
---
+++ js/contact.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+	$("form").submit(function(){
+        if(validateForm()){
+        	alert("Thank you! for your words");
+        }
+        else{
+        	return	false;
+        }
+	});
+});
+
+function validateForm(){
+
+	var name_regex = /[A-Za-z]{3,20}/;
+
+	var email_regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	
+	var name 		= 		$("#name").val();
+	var email		=   	$("#email").val();
+	var feedbacka		= 	$("#feedbacka").html();
+
+	var bool_val = 0;
+	var validationSummary = "";
+
+	if( (name != '')  && (name !=null)  && (name_regex.test(name))){
+		bool_val = 1;
+	}
+	else{
+		validationSummary += "Please enter name in correct format. </br>";	
+		bool_val = 0;
+	}
+
+	if( (email != '')  && (email_regex.test(email))){
+		bool_val = 1;
+	}else{
+		validationSummary += "Please enter email  in correct format.</br>";
+		bool_val = 0;
+		
+	}
+
+	if( (feedbacka != '')  && (feedbacka !=null)){
+		bool_val = 1;
+	}else{
+		validationSummary +="Please enter feedback in correct format.</br>";
+		bool_val = 0;
+	}
+
+	alert(bool_val);
+
+	if(bool_val == 0){
+		$(".formOutput").html(validationSummary);
+		validationSummary="";
+		return  false;
+	}
+	else{
+		return true;
+	}
+
+
+
+}
